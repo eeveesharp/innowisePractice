dd14f6d29840739ba0684202b8673034945f320c
max <maks.shamig.91@mail.ru>
2022-06-30 11:27:05 +0300

    added swager doc

--- Shop/Shop/Program.cs
+++ Shop/Shop/Program.cs
@@ -1,25 +1,31 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo{ Title = "Api", Version = "v1", Description = "A simple example ASP.NET Core Web API" });
+
+    c.SwaggerDoc("v2", new OpenApiInfo { Title = "Api2", Version = "v2" });
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
 
-app.UseAuthorization();
+    c.SwaggerEndpoint("/swagger/v2/swagger.json", "My API V2");
 
-app.MapControllers();
+    c.RoutePrefix = string.Empty;
+});
 
 app.Run();
