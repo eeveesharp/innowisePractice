3fa6285488f4f68c1fed76ec35189e4e84f1ca9a
max <maks.shamig.91@mail.ru>
2022-06-30 18:02:40 +0300

    added CRUD

--- Shop/DAL/Implementations/ProductServices.cs
+++ Shop/DAL/Implementations/ProductServices.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL.DB;
 using DAL.Entities;
 using DAL.Interfaces;
 
@@ -10,6 +11,13 @@ namespace DAL.Implementations
 {
     internal class ProductServices : IProductServices
     {
+        private readonly ApplicationContext _applicationContext;
+
+        public ProductServices(ApplicationContext applicationContext)
+        {
+            _applicationContext = applicationContext;
+        }
+
         public void Create(Product product)
         {
             throw new NotImplementedException();
@@ -22,7 +30,11 @@ namespace DAL.Implementations
 
         public void Remove(int id)
         {
-            throw new NotImplementedException();
+            Product product = _applicationContext.Products.FirstOrDefault(p => p.Id == id);
+
+            _applicationContext.Products.Remove(product);
+
+            _applicationContext.SaveChanges();
         }
 
         public void Update(int id, Product product)
--- Shop/Shop/Controllers/WeatherForecastController.cs
+++ Shop/Shop/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Shop.BD;
 
 namespace Shop.Controllers
 {
@@ -19,15 +20,67 @@ namespace Shop.Controllers
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public List<Product> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            List<Product> resultProduct = null;
+
+            using (ApplicationContext db = new ApplicationContext())
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                Product phone = new Product { Name = "Phone", Price = 123, Quantity = 33 };
+                Product TV = new Product { Name = "TV", Price = 432, Quantity = 12 };
+                db.Products.Add(phone);
+                db.Products.Add(TV);
+                db.SaveChanges();
+                resultProduct = db.Products.ToList();
+            }
+
+            return resultProduct; 
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                Product phone = new Product { Name = "Phone", Price = 123, Quantity = 33 };
+                db.Products.Add(phone);
+                db.SaveChanges();
+                db.Products.Remove(phone);
+                db.SaveChanges();
+            }
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Update()
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                Product phone = new Product { Name = "Phone", Price = 123, Quantity = 33 };
+
+                db.Products.Add(phone);
+                db.SaveChanges();
+                phone.Price = 1;
+                db.Products.Update(phone);
+                db.SaveChanges();
+            }
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Create()
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                Product phone = new Product { Name = "Phone", Price = 123, Quantity = 33 };
+
+                db.Products.Add(phone);
+                db.SaveChanges();
+            }
+
+            return Ok();
         }
     }
 }
