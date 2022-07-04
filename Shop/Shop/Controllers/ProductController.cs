239caf45029cdf6128078392caac5a1c152e523d
eeveesharp <78380344+eeveesharp@users.noreply.github.com>
2022-07-04 15:04:19 +0300

    added business-logic (#7)
    * added business-logic
    
    * fixed
    
    * added class Validation
    
    * delete comments
    
    Co-authored-by: max <maks.shamig.91@mail.ru>

--- Shop/Shop/Controllers/ProductController.cs
+++ Shop/Shop/Controllers/ProductController.cs
@@ -1,4 +1,6 @@
+using System.Diagnostics.Eventing.Reader;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Shop.BD;
 
 namespace Shop.Controllers
@@ -11,11 +13,14 @@ namespace Shop.Controllers
 
         private readonly ApplicationContext _applicationContext;
 
+        private readonly Validation _validation;
+
         public ProductController(ILogger<ProductController> logger,
             ApplicationContext applicationContext)
         {
             _logger = logger;
             _applicationContext = applicationContext;
+            _validation = new Validation(applicationContext);
         }
 
         [HttpGet]
@@ -27,20 +32,33 @@ namespace Shop.Controllers
         }
 
         [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
+        public Product Delete(int id)
         {
+            if (!_validation.IsCorrectId(id))
+            {
+                throw new ArgumentException();
+            }
+
             var product = _applicationContext.Products.FirstOrDefault(p => p.Id == id);
 
             _applicationContext.Products.Remove(product);
 
             _applicationContext.SaveChanges();
 
-            return Ok();
+            return product;
         }
 
         [HttpPut]
         public Product Update(Product product)
         {
+            if (!_validation.IsCorrectName(product)
+                || !_validation.IsCorrectPrice(product)
+                || !_validation.IsCorrectQuantity(product)
+                || !_validation.IsCorrectId(product.Id))
+            {
+                throw new ArgumentException();
+            }
+
             _applicationContext.Products.Update(product);
 
             _applicationContext.SaveChanges();
@@ -53,6 +71,13 @@ namespace Shop.Controllers
         [HttpPost]
         public Product Create(Product product)
         {
+            if (!_validation.IsCorrectName(product)
+                || !_validation.IsCorrectPrice(product)
+                || !_validation.IsCorrectQuantity(product))
+            {
+                throw new ArgumentException();
+            }
+
             _applicationContext.Products.Add(product);
 
             _applicationContext.SaveChanges();
