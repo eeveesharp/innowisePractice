ec601e56a85d144b130476b9214cfeacf031e1ba
max <maks.shamig.91@mail.ru>
2022-07-04 15:14:15 +0300

    added layers

---
+++ Shop/Shop.DAL/Entities/ProductEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Shop.DAL.Entities
+{
+    public class ProductEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
