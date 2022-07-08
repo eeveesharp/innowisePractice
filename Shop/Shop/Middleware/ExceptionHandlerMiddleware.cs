4358819582cade1d1949926fe23398b856ba87bc
max <maks.shamig.91@mail.ru>
2022-07-08 12:10:36 +0300

    added midleware

---
+++ Shop/Shop/Middleware/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace Shop.Middleware
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _requestDelegate;
+
+        public ExceptionHandlerMiddleware(RequestDelegate requestDelegate)
+        {
+            _requestDelegate = requestDelegate;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _requestDelegate.Invoke(context);
+            }
+            catch (Exception e)
+            {
+                await HandleExceptionMessageAsync(context, e).ConfigureAwait(false);
+            }
+        }
+
+        private static Task HandleExceptionMessageAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+
+            var statusCode = HttpStatusCode.InternalServerError;
+
+            var result = JsonConvert.SerializeObject(new
+            {
+                StatusCode = statusCode,
+                ErrorMessage = exception.Message
+            });
+
+            context.Response.StatusCode = (int)statusCode;
+
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
