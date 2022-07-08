1468147f1a1dee76a74e66e1fc0c87f87bc25337
eeveesharp <78380344+eeveesharp@users.noreply.github.com>
2022-07-08 16:59:53 +0300

    added unit-test for productcontroller (#18)
    Co-authored-by: max <maks.shamig.91@mail.ru>

---
+++ Shop/UnitTestApp.Tests/Controllers/ProductControllerTest.cs
@@ -0,0 +1,93 @@
+﻿using AutoMapper;
+using FluentValidation;
+using Moq;
+using Shop.BLL.Interfaces;
+using Shop.BLL.Models;
+using Shop.Controllers;
+using Shop.DAL.Entities;
+using Shop.DAL.Interfaces;
+using Shop.Validator;
+using Shop.ViewModels.Product;
+using Shouldly;
+using UnitTestApp.Tests.Models;
+using UnitTestApp.Tests.ViewModels;
+using Xunit;
+
+namespace UnitTestApp.Tests.Controllers
+{
+    public class ProductControllerTest
+    {
+        private readonly Mock<IProductServices> _productServicesMock = new();
+
+        private readonly Mock<IMapper> _mapper = new();
+
+        private readonly Mock<IValidator<ProductValidator>> _validator = new();
+
+        [Fact]
+        public async Task GetAll_WhenControllerHasData_ReturnsValidProductsList()
+        {   //Arange
+            var expectedProducts = ValidProduct.InitListProduct;
+            var expectedProductViewModels = ValidProductViewModel.InitListProduct;
+
+            _mapper.Setup(map => map.Map<IEnumerable<ProductViewModel>>(expectedProducts)).Returns(expectedProductViewModels);
+            _productServicesMock.Setup(x => x.GetAll(default)).ReturnsAsync(expectedProducts);
+            //Act
+            var controller = new ProductController(_productServicesMock.Object, _mapper.Object);
+            var result = await controller.GetAll(default);
+            //Assert
+            expectedProductViewModels.ShouldBeEquivalentTo(result);
+        }
+
+        [Fact]
+        public async Task Add_WhenValidData_ReturnValidProduct()
+        {   //Arange
+            var addProduct = ValidProduct.ValidProductModel;
+            var addProductViewMode = ValidProductViewModel.AddProductViewModel;
+            var expectedAddProductViewModel = ValidProductViewModel.ProductViewModel;
+
+            _mapper.Setup(map => map.Map<Product>(addProductViewMode)).Returns(addProduct);
+            _mapper.Setup(map => map.Map<ShortProductViewModel>(addProduct)).Returns(expectedAddProductViewModel);
+            _productServicesMock.Setup(c => c.Create(addProduct, default)).ReturnsAsync(addProduct);
+            //Act
+            var controller = new ProductController(_productServicesMock.Object, _mapper.Object);
+            var result = await controller.Create(addProductViewMode, default);
+            //Assert
+            expectedAddProductViewModel.Name.ShouldBeEquivalentTo(result.Name);
+            expectedAddProductViewModel.Price.ShouldBeEquivalentTo(result.Price);
+            expectedAddProductViewModel.Quantity.ShouldBeEquivalentTo(result.Quantity);
+        }
+
+        [Fact]
+        public async Task Update_WhenValidData_ReturnValidProduct()
+        {//Arange
+            var addProduct = ValidProduct.ValidProductModel;
+            var addProductViewMode = ValidProductViewModel.AddProductViewModel;
+            var expectedAddProductViewModel = ValidProductViewModel.ProductViewModel;
+
+            _mapper.Setup(map => map.Map<Product>(addProductViewMode)).Returns(addProduct);
+            _mapper.Setup(map => map.Map<ShortProductViewModel>(addProduct)).Returns(expectedAddProductViewModel);
+            _productServicesMock.Setup(c => c.Update(addProduct, default)).ReturnsAsync(addProduct);
+            //Act
+            var controller = new ProductController(_productServicesMock.Object, _mapper.Object);
+            var result = await controller.Update(default, addProductViewMode, default);
+            //Assert
+            expectedAddProductViewModel.Name.ShouldBeEquivalentTo(result.Name);
+            expectedAddProductViewModel.Price.ShouldBeEquivalentTo(result.Price);
+            expectedAddProductViewModel.Quantity.ShouldBeEquivalentTo(result.Quantity);
+        }
+
+        [Fact]
+        public void Delete_WhenValidData_ReturnValidProduct()
+        {   //Arange
+            var deleteProduct = ValidProduct.ValidProductModel.Id;
+
+            _mapper.Setup(map => map.Map<Product>(ValidProduct.ValidProductModel)).Returns(ValidProduct.ValidProductModel);
+            _productServicesMock.Setup(c => c.Delete(deleteProduct, default));
+            //Act
+            var controller = new ProductController(_productServicesMock.Object, _mapper.Object);
+            Action act = () => controller.Delete(deleteProduct, default);
+            //Assert
+            act.ShouldNotThrow();
+        }
+    }
+}
---
+++ Shop/UnitTestApp.Tests/ViewModels/ValidProductViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shop.BLL.Models;
+using Shop.Validator;
+using Shop.ViewModels.Product;
+
+namespace UnitTestApp.Tests.ViewModels
+{
+    public static class ValidProductViewModel
+    {
+        public static IEnumerable<ProductViewModel> InitListProduct = new List<ProductViewModel>
+            { new() { Id = 1, Name = "Test", Price = 100, Quantity = 3 } };
+
+        public static ProductViewModel ProductViewModelValid = new() { Name = "testName", Price = 100, Quantity = 3 };
+        public static AddProductViewModel AddProductViewModel = new() { Name = "testName", Price = 100, Quantity = 3 };
+        public static ShortProductViewModel ProductViewModel = new() { Name = "testName", Price = 100, Quantity = 3 };
+    }
+}
