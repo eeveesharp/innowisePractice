464c954f2c1471756ff7f32c56c36533bc4fd365
max <maks.shamig.91@mail.ru>
2022-07-08 16:51:03 +0300

    added unit-test for productcontroller

---
+++ Shop/UnitTestApp.Tests/Models/ValidProduct.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shop.BLL.Models;
+using Shop.DAL.Entities;
+using Shop.ViewModels.Product;
+
+namespace UnitTestApp.Tests.Models
+{
+    public static class ValidProduct
+    {
+        public static IEnumerable<Product> InitListProduct = new List<Product>
+            { new() { Id = 1, Name = "Test", Price = 100, Quantity = 3 } };
+
+        public static Product ValidProductModel = new() { Name = "testName", Price = 100, Quantity = 3 };
+    }
+}
