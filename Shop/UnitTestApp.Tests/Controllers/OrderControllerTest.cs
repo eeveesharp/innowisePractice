85761fd3d1d0addb1a53658e0bb64949536146e5
eeveesharp <78380344+eeveesharp@users.noreply.github.com>
2022-07-11 13:12:48 +0300

    added unit-test for order (#19)
    Co-authored-by: max <maks.shamig.91@mail.ru>

---
+++ Shop/UnitTestApp.Tests/Controllers/OrderControllerTest.cs
@@ -0,0 +1,86 @@
+﻿using AutoMapper;
+using Moq;
+using Shop.BLL.Interfaces;
+using Shop.BLL.Models;
+using Shop.Controllers;
+using Shop.ViewModels.Order;
+using Shouldly;
+using UnitTestApp.Tests.Models;
+using UnitTestApp.Tests.ViewModels;
+using Xunit;
+
+namespace UnitTestApp.Tests.Controllers
+{
+    public class OrderControllerTest
+    {
+        private readonly Mock<IOrderServices> _orderServices = new();
+
+        private readonly Mock<IMapper> _mapper = new();
+
+        [Fact]
+        public async Task GetAll_WhenControllerHasValidData_ReturnsOrderList()
+        {
+            //Arange
+            var expectedOrders = ValidOrder.ListOrder;
+            var expectedOrderViewModels = ValidOrderViewModel.ListOrder;
+
+            _mapper.Setup(map => map.Map<IEnumerable<OrderViewModel>>(expectedOrders)).Returns(expectedOrderViewModels);
+            _orderServices.Setup(x => x.GetAll(default)).ReturnsAsync(expectedOrders);
+            //Act
+            var controller = new OrderController(_orderServices.Object, _mapper.Object);
+            var result = await controller.GetAll(default);
+            //Assert
+            expectedOrderViewModels.ShouldBeEquivalentTo(result);
+        }
+
+        [Fact]
+        public async Task Add_WhenValidData_ReturnValidOrder()
+        {   //Arange
+            var addOrder = ValidOrder.ValidOrderModel;
+            var addOrderViewModel = ValidOrderViewModel.AddOrderViewModel;
+            var expectedAddOrderViewModel = ValidOrderViewModel.ShortOrderViewModel;
+
+            _mapper.Setup(map => map.Map<Order>(addOrderViewModel)).Returns(addOrder);
+            _mapper.Setup(map => map.Map<ShortOrderViewModel>(addOrder)).Returns(expectedAddOrderViewModel);
+            _orderServices.Setup(c => c.Create(addOrder, default)).ReturnsAsync(addOrder);
+            //Act
+            var controller = new OrderController(_orderServices.Object, _mapper.Object);
+            var result = await controller.Create(addOrderViewModel, default);
+            //Assert
+            expectedAddOrderViewModel.ClientName.ShouldBeEquivalentTo(result.ClientName);
+            expectedAddOrderViewModel.FinalPrice.ShouldBeEquivalentTo(result.FinalPrice);
+        }
+
+        [Fact]
+        public async Task Update_WhenValidData_ReturnValidOrder()
+        { //Arange
+            var addOrder = ValidOrder.ValidOrderModel;
+            var addOrderViewModel = ValidOrderViewModel.AddOrderViewModel;
+            var expectedAddOrderViewModel = ValidOrderViewModel.ShortOrderViewModel;
+
+            _mapper.Setup(map => map.Map<Order>(addOrderViewModel)).Returns(addOrder);
+            _mapper.Setup(map => map.Map<ShortOrderViewModel>(addOrder)).Returns(expectedAddOrderViewModel);
+            _orderServices.Setup(c => c.Update(addOrder, default)).ReturnsAsync(addOrder);
+            //Act
+            var controller = new OrderController(_orderServices.Object, _mapper.Object);
+            var result = await controller.Update(default, addOrderViewModel, default);
+            //Assert
+            expectedAddOrderViewModel.ClientName.ShouldBeEquivalentTo(result.ClientName);
+            expectedAddOrderViewModel.FinalPrice.ShouldBeEquivalentTo(result.FinalPrice);
+        }
+
+        [Fact]
+        public void Delete_WhenValidData_ReturnValidOrder()
+        {   //Arange
+            var deleteProduct = ValidOrder.ValidOrderModel.Id;
+
+            _mapper.Setup(map => map.Map<Order>(ValidProduct.ValidProductModel)).Returns(ValidOrder.ValidOrderModel);
+            _orderServices.Setup(c => c.Delete(deleteProduct, default));
+            //Act
+            var controller = new OrderController(_orderServices.Object, _mapper.Object);
+            Action act = () => controller.Delete(deleteProduct, default);
+            //Assert
+            act.ShouldNotThrow();
+        }
+    }
+}
--- Shop/UnitTestApp.Tests/ViewModels/ValidProductViewModel.cs
+++ Shop/UnitTestApp.Tests/ViewModels/ValidProductViewModel.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Shop.BLL.Models;
-using Shop.Validator;
-using Shop.ViewModels.Product;
+﻿using Shop.ViewModels.Product;
 
 namespace UnitTestApp.Tests.ViewModels
 {
@@ -14,7 +7,6 @@ namespace UnitTestApp.Tests.ViewModels
         public static IEnumerable<ProductViewModel> InitListProduct = new List<ProductViewModel>
             { new() { Id = 1, Name = "Test", Price = 100, Quantity = 3 } };
 
-        public static ProductViewModel ProductViewModelValid = new() { Name = "testName", Price = 100, Quantity = 3 };
         public static AddProductViewModel AddProductViewModel = new() { Name = "testName", Price = 100, Quantity = 3 };
         public static ShortProductViewModel ProductViewModel = new() { Name = "testName", Price = 100, Quantity = 3 };
     }
