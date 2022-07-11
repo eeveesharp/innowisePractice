c6fc7de99c965380b90805a504efd1b038a51a48
eeveesharp <78380344+eeveesharp@users.noreply.github.com>
2022-07-11 13:13:14 +0300

    added unittest for services (#20)
    Co-authored-by: max <maks.shamig.91@mail.ru>

---
+++ Shop/Bll.Test/Etities/ValidProductEntity.cs
@@ -0,0 +1,12 @@
+﻿using Shop.DAL.Entities;
+
+namespace Bll.Test.Etities
+{
+    public class ValidProductEntity
+    {
+        public static IEnumerable<ProductEntity> ProductEntityList = new List<ProductEntity>
+            { new() { Id = 1, Name = "Test", Price = 100, Quantity = 3 } };
+
+        public static ProductEntity ProductEntity = new() { Id = 1, Name = "Test", Price = 100, Quantity = 3 };
+    }
+}
