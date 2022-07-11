b193d458c990a57fc0c8ff1cf71dd9b522399e4e
max <maks.shamig.91@mail.ru>
2022-07-11 13:25:44 +0300

    added unit test for orderServices

---
+++ Shop/Bll.Test/Etities/ValidOrderEntity.cs
@@ -0,0 +1,12 @@
+﻿using Shop.DAL.Entities;
+
+namespace Bll.Test.Etities
+{
+    public class ValidOrderEntity
+    {
+        public static IEnumerable<OrderEntity> ListOrderEntity = new List<OrderEntity>
+            { new() { Id = 1, ClientName = "bob",FinalPrice = 1000} };
+
+        public static OrderEntity OrderEntity = new() { ClientName = "bob", FinalPrice = 1000 };
+    }
+}
---
+++ Shop/Bll.Test/Services/OrderServicesTest.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+using Bll.Test.Etities;
+using Bll.Test.Models;
+using Moq;
+using Shop.BLL.Models;
+using Shop.BLL.Services;
+using Shop.DAL.Entities;
+using Shop.DAL.Interfaces;
+using Shouldly;
+using Xunit;
+
+namespace Bll.Test.Services
+{
+    public class OrderServicesTest
+    {
+        private readonly Mock<IOrderRepository> _orderRepositoryMock = new();
+
+        private readonly Mock<IMapper> _mapperMock = new();
+
+        [Fact]
+        public async Task GetAll_WhenOrderServiceHasData_ReturnsOrderEntityList()
+        {
+            _mapperMock.Setup(map => map.Map<IEnumerable<Order>>(ValidOrderEntity.ListOrderEntity)).Returns(ValidOrder.ListOrder);
+            _orderRepositoryMock.Setup(s => s.GetAll(default)).ReturnsAsync(ValidOrderEntity.ListOrderEntity);
+
+            var OrderServices = new OrderServices(_orderRepositoryMock.Object, _mapperMock.Object);
+
+            var result = await OrderServices.GetAll(default);
+
+            ValidOrder.ListOrder.ShouldBeEquivalentTo(result);
+        }
+
+        [Fact]
+        public async Task Create_WhenOrderServiceHasValidData_ReturnsOrderEntity()
+        {
+            _mapperMock.Setup(map => map.Map<Order>(ValidOrderEntity.OrderEntity)).Returns(ValidOrder.ValidOrderModel);
+            _mapperMock.Setup(map => map.Map<OrderEntity>(ValidOrder.ValidOrderModel)).Returns(ValidOrderEntity.OrderEntity);
+            _orderRepositoryMock.Setup(s => s.Create(ValidOrderEntity.OrderEntity, default)).ReturnsAsync(ValidOrderEntity.OrderEntity);
+
+            var OrderServices = new OrderServices(_orderRepositoryMock.Object, _mapperMock.Object);
+
+            var result = await OrderServices.Create(ValidOrder.ValidOrderModel, default);
+
+            ValidOrder.ValidOrderModel.ShouldBeEquivalentTo(result);
+        }
+
+        [Fact]
+        public async Task Update_WhenOrderServiceHasValidData_ReturnsOrderEntity()
+        {
+            _mapperMock.Setup(map => map.Map<Order>(ValidOrderEntity.OrderEntity)).Returns(ValidOrder.ValidOrderModel);
+            _mapperMock.Setup(map => map.Map<OrderEntity>(ValidOrder.ValidOrderModel)).Returns(ValidOrderEntity.OrderEntity);
+            _orderRepositoryMock.Setup(s => s.Update(ValidOrderEntity.OrderEntity, default)).ReturnsAsync(ValidOrderEntity.OrderEntity);
+
+            var OrderServices = new OrderServices(_orderRepositoryMock.Object, _mapperMock.Object);
+
+            var result = await OrderServices.Update(ValidOrder.ValidOrderModel, default);
+
+            ValidOrder.ValidOrderModel.ShouldBeEquivalentTo(result);
+        }
+
+        [Fact]
+        public async Task Delete_WhenValidData_ReturnValidOrderEntity()
+        {
+            _mapperMock.Setup(map => map.Map<Order>(ValidOrderEntity.OrderEntity)).Returns(ValidOrder.ValidOrderModel);
+            _orderRepositoryMock.Setup(s => s.Delete(ValidOrderEntity.OrderEntity, default));
+
+            var OrderServices = new OrderServices(_orderRepositoryMock.Object, _mapperMock.Object);
+
+            Action action = async () => await OrderServices.Delete(ValidOrder.ValidOrderModel.Id, default);
+
+            action.ShouldNotThrow();
+        }
+    }
+}
