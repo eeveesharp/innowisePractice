ae99d7aa98e11cbe3b323ef2cb893a5c47ae0c0f
max <maks.shamig.91@mail.ru>
2022-07-14 18:16:06 +0300

    fixed test

--- Shop/UnitTestApp.Tests/ViewModels/ValidOrderViewModel.cs
+++ Shop/UnitTestApp.Tests/ViewModels/ValidOrderViewModel.cs
@@ -5,24 +5,23 @@ namespace UnitTestApp.Tests.ViewModels
     public class ValidOrderViewModel
     {
         public static IEnumerable<OrderViewModel> ListOrder = new List<OrderViewModel>
-            { new() { Id = 1, Quantity = 5, TotalPrice = 100, 
+            { new() { Id = 1, Quantity = 5, TotalPrice = 100,
                 Client = new() { LastName = "test", Name = "test" },
                 Product = new(){Quantity = 3, Name = "test",Price = 100}}
             };
-        
+
         public static AddOrderViewModel AddOrderViewModel = new()
         {
             ClientId = 1,
             ProductId = 1,
             Quantity = 3,
-            ClientViewModel = new() { Id = 1, LastName = "test", Name = "test" }
         };
 
         public static ShortOrderViewModel ShortOrderViewModel = new()
         {
             TotalPrice = 100,
             Quantity = 3,
-            Client = new (){ LastName = "Test", Name = "Test" }
+            Client = new() { LastName = "Test", Name = "Test" }
         };
     }
 }
