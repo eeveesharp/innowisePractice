70fee6c3b0ecfdd724d3a4d43405f5d1315b0ca6
max <maks.shamig.91@mail.ru>
2022-07-14 12:56:52 +0300

    deleted imp getall and get in orderServices

--- Shop/Shop.BLL/Services/OrderServices.cs
+++ Shop/Shop.BLL/Services/OrderServices.cs
@@ -17,23 +17,5 @@ namespace Shop.BLL.Services
             _repository = repository;
             _mapper = mapper;
         }
-
-        public override async Task<Order> Get(int id, CancellationToken ct)
-        {
-            var resultOrderEntity = await _repository.Get(id, ct);
-
-            var resultOrder = _mapper.Map<Order>(resultOrderEntity);
-
-            return resultOrder;
-        }
-
-        public override async Task<IEnumerable<Order>> GetAll(CancellationToken ct)
-        {
-            var resultOrderEntityList = await _repository.GetAll(ct);
-
-            var resultOrderList = _mapper.Map<IEnumerable<Order>>(resultOrderEntityList);
-
-            return resultOrderList;
-        }
     }
 }
